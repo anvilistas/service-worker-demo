3c421eea01276e404ca51e5b41f93521a6d5a0c1
stu <stu@anvil.works>
2023-11-28 16:56:12 +0800

    Use new api

--- client_code/service_worker.py
+++ client_code/service_worker.py
@@ -3,15 +3,11 @@ from .model.client import TodoStore
 store = TodoStore()
 
 
-@self.sync_event_handler
-def onsync(e):
-    if e.tag != "todos.online":
-        return
+def onsync():
     self.raise_event("todos.sync.starting")
     store.sync()
     self.raise_event("todos.sync.done")
 
-
-self.onsync = onsync
+self.BackgroundSync("todos.online", onsync)
 
 print("Service worker loaded")
